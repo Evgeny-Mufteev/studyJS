c73109191501167e078ff9d2e6614ab38db8abbb
Evgeny Mufteev <evgenymufteev@gmail.com>
2021-09-25 12:31:52 +0300

    мой первый учебный проект

--- main.js
+++ main.js
@@ -10,13 +10,46 @@
 // console.log(isPaymentCash);
 // console.log(fullName);
 
-
 // let userName = null;
 // console.log(userName);
 
 // // let userName2;
 // // alert(userName2);
 
-let num = 2;
-string = String(num);
-console.log(string)
+// let num = 2;
+// string = String(num);
+// console.log(string);
+
+// let isCash = true;
+// string = String(isCash);
+// console.log(string);
+
+// let number = 45;
+// numberToString = String(number);
+// console.log(numberToString);
+
+// let string = '2';
+// num = Number(string);
+// console.log(num);
+
+// let string;
+// num = Number(string);
+// console.log(num);
+
+// let age = null;
+// num = Number(age);
+// console.log(num);
+
+// let string = " 123  n ";
+// num = Number(string);
+// console.log(num);
+
+// let testBoolean = '1';
+// boolean = Boolean(testBoolean);
+// console.log(boolean);
+
+// console.log(7 % 4)
+
+// console.log(+'56')
+
+console.log(42 + "fgh ");
