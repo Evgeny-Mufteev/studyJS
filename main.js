dd7f29848230ae0f0d7f819a1748c732b31fd8bc
Evgeny Mufteev <EvgenyMufteev@gmai.com>
2021-09-25 09:45:12 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+// const namePlanet = 'Земля';
+// let userTime = 142;
+// let countArticles = 42;
+// const isPaymentCash = 'true';
+// const fullName = "Иванов Иван Иванович";
+
+// console.log(namePlanet);
+// console.log(userTime);
+// console.log(countArticles);
+// console.log(isPaymentCash);
+// console.log(fullName);
+
+
+// let userName = null;
+// console.log(userName);
+
+// // let userName2;
+// // alert(userName2);
+
+let num = 2;
+string = String(num);
+console.log(string)
