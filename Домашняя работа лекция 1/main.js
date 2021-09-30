20880a4d88be3661d4ace97eff219a221cc07418
Evgeny Mufteev <evgenymufteev@gmail.com>
2021-09-30 21:51:34 +0300

    добавил общую структуру папок

---
+++ Домашняя работа лекция 1/main.js
@@ -0,0 +1,85 @@
+// console.log('Hello World');
+
+// const age = 2021;
+// console.log(age);
+
+// const userName = 'Ivan';
+// console.log(userName)
+
+// const isCash = false;
+// console.log(isCash)
+
+// const paymant = null;
+// console.log(paymant)
+
+// let polyfil
+// console.log(polyfil)
+// при объявлении const значение должно присваиваться сразу
+
+// const user = {
+//     nane: 'Ivan',
+//     age: 25
+// };
+// console.log(user)
+
+// const id = Symbol("id");
+// console.log(id)
+
+// const bigint = 9n * 9n;
+// console.log(bigint)
+
+
+// Попробовать изменить переменную объявленную через const
+// const fullName = 'Ivanov Ivan';
+// fullName = 'Petrov Alex';
+// console.log(fullName)
+
+
+// Попробовать изменить переменную с типом object объявленную через const
+// const fullName = {
+//     userName: 'Ivan',
+//     lastName: 'Ivanov'
+// };
+// console.log(fullName)
+
+// fullName = {
+//     userName: 'Alex',
+//     lastName: 'Petrov'
+// }
+// console.log(fullName)
+// Изменить переменную const пока нельзя зато можно изменить ключи в объекте:
+// const fullName = {
+//     userName: 'Ivan',
+//     lastName: 'Ivanov'
+// };
+// console.log(fullName)
+
+// fullName.userName = 'Petr';
+// console.log(fullName)
+
+
+// Попробовать изменить переменную с типом object объявленную через let
+// let fullName = {
+//     userName: 'Ivan',
+//     lastName: 'Ivanov'
+// };
+// console.log(fullName)
+
+// fullName = {
+//     userName: 'Alex',
+//     lastName: 'Petrov'
+// }
+// console.log(fullName)
+
+// Попробовать изменить переменную с типом object объявленную через var
+// var fullName = {
+//     userName: 'Ivan',
+//     lastName: 'Ivanov'
+// };
+// console.log(fullName)
+
+// fullName = {
+//     userName: 'Alex',
+//     lastName: 'Petrov'
+// }
+// console.log(fullName)
